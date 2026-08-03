7427a4ddc87b1f41e209b1fda691beeba600034c
Starweaverlumina <christopherwicker402@gmail.com>
2026-08-03 11:16:36 -0400

    Fix WPF and framework type ambiguities

---
+++ src/SightForge/GlobalAliases.cs
@@ -0,0 +1,5 @@
+global using Brush = System.Windows.Media.Brush;
+global using TextBox = System.Windows.Controls.TextBox;
+global using MemoryStream = System.IO.MemoryStream;
+global using BinaryWriter = System.IO.BinaryWriter;
+global using HttpClient = System.Net.Http.HttpClient;
