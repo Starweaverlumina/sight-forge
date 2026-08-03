8ca0300fbc4a1d44078de9866467b8a48f827ba0
Starweaverlumina <christopherwicker402@gmail.com>
2026-08-03 13:14:50 -0400

    Persist SoundForge announcement confidence preference

--- src/SightForge/Models/AudioDirectionModels.cs
+++ src/SightForge/Models/AudioDirectionModels.cs
@@ -32,6 +32,7 @@ public sealed class SoundForgeSettings
 {
     public bool Enabled { get; set; }
     public bool MonoListeningEnabled { get; set; }
+    public bool AnnounceOnlyHighConfidence { get; set; } = true;
     public double MinimumAudibleLevel { get; set; } = 0.015;
     public double DirectionDeadZone { get; set; } = 0.12;
     public int AnalysisWindowMilliseconds { get; set; } = 80;
